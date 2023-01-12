d5262a48ab1e5d72ad41525b7b7e25bfdbf6eafd
Jared Lee <jared.lee@intellitect.com>
2023-01-12 15:59:58 -0800

    Fixes compile and runtime issues.

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -19,12 +19,15 @@ namespace PrincessBrideTrivia
                     numberCorrect++;
                 }
             }
+
+            
             Console.WriteLine("You got " + GetPercentCorrect(numberCorrect, questions.Length) + " correct");
         }
 
         public static string GetPercentCorrect(int numberCorrectAnswers, int numberOfQuestions)
         {
-            return (numberCorrectAnswers / numberOfQuestions * 100) + "%";
+            double percentCorrect = (double)numberCorrectAnswers / numberOfQuestions * 100;
+            return (int)percentCorrect + "%";
         }
 
         public static bool AskQuestion(Question question)
@@ -89,6 +92,8 @@ namespace PrincessBrideTrivia
                 question.Answers[1] = answer2;
                 question.Answers[2] = answer3;
                 question.CorrectAnswerIndex = correctAnswerIndex;
+
+                questions[i] = question;
             }
             return questions;
         }
